dbcb3bde484b6b20cd52d3b8a41b3ad276eead73
lailahgrant <lailahgrant19@gmail.com>
2021-11-30 10:25:32 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>hello</h1>
+    <h2>lailah</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
