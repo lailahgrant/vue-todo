26886ca140e713878cbc543342541d5abfc73cf8
lailahgrant <lailahgrant19@gmail.com>
2021-11-30 12:23:36 +0300

    form and table to display data entered
    state helper functions and ..Api.js file imports

--- src/components/UserComponent.vue
+++ src/components/UserComponent.vue
@@ -1,3 +1,19 @@
+<script>
+
+// import Api file
+import AppApi from '../services/AppApi'
+
+//import helper functions of vuex to retrieve and set data from store.
+import { mapMutations, mapState, mapGetters } from 'vuex'
+
+export default {
+    
+}
+
+
+</script>
+
+
 <template>
     
     <div>
@@ -37,7 +53,28 @@
                 <button @click="addUserInput">Add</button>
 
             </div>
+
+            <div class="displaytable">
+                <div class="header">
+                    <p>Name</p>
+                    <p>Email</p>
+                    <p>Phone</p>
+                    <p>Website</p>
+                </div>
+
+                <div class="row" v-for="(user, index) in users" :key="user.id" >
+                    <div>  {{user.name}} </div>
+                    <div> <a href='"mailto:" +user.email'>{{ user.email}} </a> </div>
+                    <div> {{user.phone}} </div>
+                    <div> {{user.website}}</div>
+                    <span @click="removeUser(index)" class="link" >Delete</span>
+                </div>
+
+            </div>
+
         </div>
+
+        <p>Total users in store : <strong> {{ this.totalUsers }} </strong></p>
 
     </div>
 
@@ -48,18 +85,21 @@
 .container {
     margin-left:5rem;
     width:80%;
+}
+.displaytable{
+    width:100%;
+    margin-top: 1rem;
     
 }
-
 .row {
   display: flex;
   justify-content:flex-start;
   flex-direction:row;
   width:100%;
-  padding:5px;
+  padding:10px;
 }
 .addcontainer {
-  width: 300px;
+  width: 20rem;
 }
 .header {
     display: flex;
@@ -73,7 +113,11 @@
     height:48px;
     line-height: 48px; 
 }
-.row > div , .header > div{
+
+/* .header > p{
+    margin-bottom: 1rem;
+} */
+.row > div , .header > p{
   width :19%;
   text-align:left;
   word-wrap: break-word;
--- src/store.js
+++ src/store.js
@@ -3,11 +3,14 @@ import Vue from 'vue'
 
 Vue.use(Vuex)
 
+
+//add mutations, getters and state
 export default new Vuex.Store({
 
     state: {
         //state : It holds all the data for our application
         
+        //display a list of users
     },
 
     getters: {
